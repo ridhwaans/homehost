feb0e1777baa227aa9395a0256037b30b3d40ec3
Rid Shakeel <shakeel.ridhwaan@gmail.com>
2021-05-11 14:28:36 -0400

    todo cut down api client

--- server.js
+++ server.js
@@ -36,8 +36,8 @@ watcher.on('ready', function() {
       .map(groups => groups
       .filter(group => typeof group == "object")
       .map(files => files
-      .map(file => groups[0] + "/" + file))) // first entry will be the folder name to concatenate
-      .flat().flat()
+      .map(file => `${groups[0]}/${file}`))) // first entry will be the folder name to concatenate
+      .flat(Infinity)
       .filter(x => !Object.keys(collection).includes(x));
   //console.table(files);
 })
@@ -153,9 +153,10 @@ if (process.env.NODE_ENV == 'prod'){
 }
 
 var generateMetaData = function(){
+  //generateMovieMetaData()
   //generateMusicMetaData()
   //generateTVMetaData()
-  [generateMovieMetaData()].forEach(async (metadata) => {
+  [generateMovieMetaData(), generateTVMetaData(), generateMusicMetaData()].forEach(async (metadata) => {
     await metadata;
   });
   console.log("Finished async");
@@ -168,41 +169,36 @@ const generateTVMetaData = async () => {
         json = { tv: [] };
 
   console.log('Generating data for TV...')
-  let subdirs = await new Promise((resolveSubdirs) => {
-    node_dir.subdirs(config.tv.path, async (err, subdirs) => {
-      if (err) throw err;
-      resolveSubdirs(subdirs);
-    });
-  });
 
-  await bluebird.mapSeries(subdirs, async (subdir) => {
-    console.log('GET: ' + subdir);
+  let tv_shows = Object.keys(collection)
+    .filter(group => typeof group == "string")
+    .filter(group => group.startsWith(config.tv.path) && group != config.tv.path);
+
+  await bluebird.mapSeries(tv_shows, async (tv_show) => {
+    console.log('GET: ' + tv_show);
     // find tv show on TMDb
-    let tv_id = parseInt(subdir.match(re)[1])
+    let tv_id = parseInt(tv_show.match(re)[1])
     let show = await tvClient.send(new lib.requests.TVShow(tv_id), 250, null)
     show.seasons.forEach(function(season, i) {
       show.seasons[i].episodes = []
     });
 
-    let files = await new Promise((resolveFiles) => {
-      node_dir.files(subdir, (err, files) => {
-        if (err) throw err;
-        resolveFiles(files);
-      });
-    });
-
-    await bluebird.mapSeries(files, async (file) => {
-      console.log('GET: ' + file);
+    let episode_files = files.filter(x => x.startsWith(config.tv.path) && x.includes(tv_id))
+    await bluebird.mapSeries(episode_files, async (episode_file) => {
+      console.log('GET: ' + episode_file);
       // find tv episode on TMDb
-      let season_number = parseInt(file.match(re2)[1])
-      let episode_number = parseInt(file.match(re2)[2])
-      
-      let episode = await tvClient.send(new lib.requests.TVEpisode(tv_id, season_number, episode_number), 250, null)
-      episode.fs_path = file;
-      episode.url_path = ('http://localhost:' + port + '/tv/').concat(tv_id, '/', episode.season_number, '/', episode.episode_number);
+      let season_number = parseInt(episode_file.match(re2)[1])
+      let episode_number = parseInt(episode_file.match(re2)[2])
+      try {
+        let episode = await tvClient.send(new lib.requests.TVEpisode(tv_id, season_number, episode_number), 250, null)
+        episode.fs_path = episode_file;
+        episode.url_path = `http://localhost:${port}/tv/${tv_id}/${episode.season_number}/${episode.episode_number}`;
 
-      let seasonIndex = _.findIndex(show.seasons, { season_number: season_number });
-      show.seasons[seasonIndex].episodes.push(episode);
+        let seasonIndex = show.seasons.findIndex(season => season.season_number == season_number.toString()); 
+        show.seasons[seasonIndex].episodes.push(episode);
+      } catch(e) {
+        return true; // skip the episode if there is a problem fetching metadata
+      }
     });
     json.tv.push(show);
   });
@@ -227,7 +223,7 @@ var generateMovieMetaData = function() {
   return moviesClient.send(new lib.requests.Movie(file.match(re)[1]), 250, null)
     .then((movie) => {
     movie.fs_path = file;
-    movie.url_path = 'http://localhost:' + port + '/movies/' + movie.id;
+    movie.url_path = `http://localhost:${port}/movies/${movie.id}`;
     json.movies.push(movie);
     });
   })
@@ -247,6 +243,93 @@ var generateMovieMetaData = function() {
 };
 
 var generateMusicMetaData = function() {
+  return new Promise(function(resolve, reject) {
+
+  var re = new RegExp(/(\w+)$/); // album_id
+      re2 = new RegExp(/((\d+)-)?(\d+)/); // disc_number - track_number
+      json = { music: [] };
+
+  console.log('Generating data for Music...')
+
+  let album_dirs = Object.keys(collection)
+    .filter(group => typeof group == "string")
+    .filter(group => group.startsWith(config.music.path) && group != config.music.path);
+        
+    bluebird.mapSeries(album_dirs, function(album_dir){
+      console.log('GET: ' + album_dir);
+      let track_files = [];
+
+      if (album_dir.toUpperCase().endsWith("UNKNOWN ALBUM")){
+        // build music album not on Spotify
+        let album = {
+          id: 'unknown',
+          name: 'Unknown Album',
+          artists: [{name: 'Various Artists'}],
+          images: [{url: 'http://i.imgur.com/bVnx0IY.png'}],
+          release_date: 'NaN',
+          label: 'Various Labels',
+          tracks: {items:[]}
+        }
+        track_files = files.filter(x => x.toUpperCase().includes("UNKNOWN ALBUM"))
+
+        track_files.forEach( function( track_file, index ) {
+          track = track_file.replace(/^.*[\\\/]/, ''); // get filename from full filepath
+          console.log('GET: ' + track);
+
+          let item = {}
+          item.id = index
+          item.name = track.replace(/.mp3|.flac/gi,'')
+          item.disc_number = 1
+          item.track_number = index + 1
+          item.duration_ms = 'NaN'
+          item.fs_path = track_file
+          item.url_path = `http://localhost:${port}/music/${album.id}/${item.disc_number}/${item.track_number}`
+          item.external_urls = {spotify: null}
+          album.tracks.items.push(item)
+        });
+
+        json.music.push(album);
+      } else {
+        // find music album on Spotify
+        return musicClient.send(new lib.requests.Album(album_dir.match(re)[1]), 50, null)
+        .then((album) => {
+          // remove unnecessary Spotify json
+          delete album.available_markets;
+          for(var i=0;i<album.tracks.items.length;i++){
+            delete album.tracks.items[i].artists;
+            delete album.tracks.items[i].available_markets;}
+
+          track_files = files.filter(x => x.includes(album_dir.match(re)[1]))
+          track_files.forEach( function( track_file, index ) {
+              track = track_file.replace(/^.*[\\\/]/, ''); // get filename from full filepath
+              console.log('GET: ' + track);
+              
+              // for each track in the Spotify music album
+              album.tracks.items.forEach(function(item) {
+                // if track found
+                if ( (item.disc_number == parseInt(track.match(re2)[1] || 1) ) && 
+                  (item.track_number == parseInt(track.match(re2)[3]) ) ) {
+                  item.fs_path = track_file; 
+                  item.url_path = `http://localhost:${port}/music/${album.id}/${item.disc_number}/${item.track_number}`;
+                }
+              });
+            });
+
+          json.music.push(album);
+        });
+      }
+    }) // end of .mapSeries()
+    .then(function(music){
+      fs.writeFile('./music.json', JSON.stringify(json, 0, 4), 'utf8', (err)=>{
+        if(err) console.log(err)
+        else console.log('[MUSIC] File saved');
+        resolve(json);
+      })
+    })
+    .catch(function(err){
+      console.log('Music metadata could not be generated due to some error', err);
+    });
+  });
 };
 
 console.log(figlet.textSync('homehost', 
