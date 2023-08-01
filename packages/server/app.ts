354565d91b51b99cfd1ba7877d9ed4b14d6fb1b4
Abdur Rahman <abdur@lycus.com>
2023-08-02 02:24:31 +0500

    moved app related code to app.ts

---
+++ packages/server/app.ts
@@ -0,0 +1,20 @@
+import path from 'path';
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+
+import bodyParser from 'body-parser';
+import { NODE_ENV, PROD_ENV } from './constants';
+
+export const app = express();
+
+if (NODE_ENV == PROD_ENV) {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.use(cors());
+app.use(helmet());
+app.use(bodyParser.json());
+
+// routes
+app.use('/', require('./routes'));
--- packages/server/server.ts
+++ packages/server/server.ts
@@ -1,27 +1,10 @@
-import path from 'path';
-import express from 'express';
-import cors from 'cors';
-import helmet from 'helmet';
-
 import figlet from 'figlet';
-import bodyParser from 'body-parser';
 
 import { fileWatcher } from './jobs';
-import { NODE_ENV, PORT, PROD_ENV } from './constants';
+import { PORT } from './constants';
+import { app } from './app';
 
 fileWatcher();
-
-const app = express();
-
-if (NODE_ENV == PROD_ENV) {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.use(cors());
-app.use(helmet());
-
-app.use(bodyParser.json());
-app.use('/', require('./routes'));
 
 console.log(figlet.textSync('homehost'));
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
