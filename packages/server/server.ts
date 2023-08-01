8b9dad765785503187bf59fcf7123eb7778a03e2
Abdur Rahman <abdur@lycus.com>
2023-08-02 01:52:54 +0500

    cors package is used.

--- packages/server/server.ts
+++ packages/server/server.ts
@@ -1,29 +1,24 @@
-const path = require('path');
-const express = require('express');
+import path from 'path';
+import express from 'express';
+import cors from 'cors';
 
-const figlet = require('figlet');
-const bodyParser = require('body-parser');
+import figlet from 'figlet';
+import bodyParser from 'body-parser';
+import { configDotenv } from 'dotenv';
 
 const app = express();
 const port = process.env.PORT || 5000;
-require('dotenv').config();
-require('./jobs').fileWatcher();
+import { fileWatcher } from './jobs';
+configDotenv();
 
-// Serve the static files from the React app
+fileWatcher();
+
 if (process.env.NODE_ENV == 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-// CORS
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', process.env.CLIENT_BASE_URL); // update to match the domain you will make the request from
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
-});
 
-// Include routes
+app.use(cors());
+
 app.use(bodyParser.json());
 app.use('/', require('./routes'));
 
