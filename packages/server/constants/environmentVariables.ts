a64fc9c7148d5def98743d58cd210d6a3bbf7e70
Abdur Rahman <abdur@lycus.com>
2023-08-02 02:14:25 +0500

    PORT, NODE_ENV is added to env vars.

--- packages/server/constants/environmentVariables.ts
+++ packages/server/constants/environmentVariables.ts
@@ -1,7 +1,10 @@
 import { configDotenv } from 'dotenv';
+import { DEV_ENV } from './constants';
 
 configDotenv();
 
+export const PORT = process.env.PORT || 5000;
+export const NODE_ENV = process.env.NODE_ENV || DEV_ENV;
 export const DISABLE_SYNC_ENV = process.env.DISABLE_SYNC ?? false;
 export const MOVIES_PATH_ENV = process.env.MOVIES_PATH ?? '';
 export const TV_PATH_ENV = process.env.TV_PATH ?? '';
