6b93348d9fc2a2271de775d206ae800883c885c2
Abdur Rahman <abdur@lycus.com>
2023-08-02 15:52:31 +0500

    DATABASE_URL_ENV is added to ENV vars

--- packages/server/constants/environmentVariables.ts
+++ packages/server/constants/environmentVariables.ts
@@ -5,6 +5,7 @@ configDotenv();
 
 export const PORT = process.env.PORT || 5000;
 export const NODE_ENV = process.env.NODE_ENV || DEV_ENV;
+export const DATABASE_URL_ENV = process.env.DATABASE_URL || '';
 export const DISABLE_SYNC_ENV = process.env.DISABLE_SYNC ?? false;
 export const MOVIES_PATH_ENV = process.env.MOVIES_PATH ?? '';
 export const TV_PATH_ENV = process.env.TV_PATH ?? '';
