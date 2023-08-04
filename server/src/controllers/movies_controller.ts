749acf2646df4df1c30fa34b4ac9db89d34ca663
Abdur Rahman <abdur@lycus.com>
2023-08-05 00:19:31 +0500

    MoviesController extends BaseController.

--- server/src/controllers/movies_controller.ts
+++ server/src/controllers/movies_controller.ts
@@ -1,10 +1,13 @@
 import { Request, Response } from 'express';
+import { BaseController } from './base_controller';
 import { OK } from '../constants/statusCodes';
 
-class MoviesController {
+class MoviesController extends BaseController {
+  constructor() {
+    super();
+  }
   index = (req: Request, res: Response) => {
-    res.status(OK);
-    res.json({ status: OK });
+    this.sendJson(req, res, OK, true, { from: 'MoviesController#index' });
   };
 }
 
