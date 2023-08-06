9281812582d569f0ffe2b7dd66e2ffb7366fae2d
Abdur Rahman <abdur@lycus.com>
2023-08-06 15:42:51 +0500

    routes for CRUD in movies are added.

--- server/src/routes/movies_router.ts
+++ server/src/routes/movies_router.ts
@@ -4,3 +4,7 @@ import { moviesController } from '../controllers';
 export const moviesRouter = express.Router();
 
 moviesRouter.get('/', moviesController.index);
+moviesRouter.get('/:tmdb_id', moviesController.show);
+moviesRouter.post('/', moviesController.create);
+moviesRouter.patch('/:tmdb_id', moviesController.update);
+moviesRouter.delete('/:tmdb_id', moviesController.delete);
