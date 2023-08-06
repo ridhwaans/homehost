bbb0358f9118dce96c1c29076f75a8ce7e025915
Abdur Rahman <abdur@lycus.com>
2023-08-06 20:49:44 +0500

    multer & movies service is created.

--- server/src/services/movies_service.ts
+++ server/src/services/movies_service.ts
@@ -1,13 +1,33 @@
-import {Request, Response} from "express";
+import { Request, Response } from 'express';
 import { NotAvailableModel } from '../db';
-import {MOVIE} from "../constants"
+import { uploadVedio } from './multer';
+import { ServiceReturnType } from '../types';
+import { MOVIE, UPLOADE_DIR } from '../constants';
+import path from 'path';
+// import { MOVIE } from '../constants';
 
-export const createMovieService = async (req: Request, res: Response) => {
-  const {title, imdb_id} = req.params;
-
-  if()
-
-  const notAvailableObj = NotAvailableModel.create({
-    data: { title, imdb_id, type: MOVIE, fs_path: },
+export const createMovieService = async (
+  req: Request,
+  res: Response
+): Promise<ServiceReturnType> => {
+  let response: ServiceReturnType = { success: true, error: {}, data: {} };
+  const vedioFile = (req.files as any)['vedioFile'][0];
+  const { title, imdb_id } = req.body;
+  console.log(title);
+  console.log(imdb_id);
+  // console.log('fieldname', vedioFile.fieldname);
+  // console.log('originalname', vedioFile.originalname);
+  // console.log('destination', vedioFile.destination);
+  // console.log('filename', vedioFile.filename);
+  // console.log('path', vedioFile.path);
+  const obj = await NotAvailableModel.create({
+    data: {
+      fs_path: path.join(UPLOADE_DIR, vedioFile.filename),
+      type: MOVIE,
+      title,
+      imdb_id,
+    },
   });
+  response.data = obj;
+  return response;
 };
