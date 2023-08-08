95240549df9a10bf92f16c1ff24d92906d7126a3
Abdur Rahman <abdur@lycus.com>
2023-08-08 16:46:14 +0500

    PORT is converted to int value instead of string.

--- server/src/constants/environmentVariables.ts
+++ server/src/constants/environmentVariables.ts
@@ -2,8 +2,7 @@ import { configDotenv } from 'dotenv';
 import { DEV_ENV } from './constants';
 
 configDotenv();
-
-export const PORT = process.env.PORT || 5000;
+export const PORT = process.env.PORT ? parseInt(process.env.PORT) : 5000;
 export const NODE_ENV = process.env.NODE_ENV || DEV_ENV;
 export const DATABASE_URL_ENV = process.env.DATABASE_URL || '';
 export const DISABLE_SYNC_ENV = process.env.DISABLE_SYNC ?? false;
